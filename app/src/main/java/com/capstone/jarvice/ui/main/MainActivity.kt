1387649d827de58af283a3a8feeacd41afaaaf66
Maulana Husein <maulanahusein03@gmail.com>
2022-12-01 17:55:45 +0700

    Initial commit

---
+++ app/src/main/java/com/capstone/jarvice/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.jarvice.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.jarvice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
